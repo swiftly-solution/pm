777c143d140264166bafaf3fc28b74069432e49d
Andrei Sebastian <andreieurt@gmail.com>
2024-07-11 20:19:13 +0300

    upload(src): First Upload

---
+++ README.md
@@ -0,0 +1,49 @@
+<p align="center">
+  <a href="https://github.com/swiftly-solution/pm">
+    <img src="https://cdn.swiftlycs2.net/swiftly-logo.png" alt="SwiftlyLogo" width="80" height="80">
+  </a>
+
+  <h3 align="center">[Swiftly] Private Messages</h3>
+
+  <p align="center">
+    A Swiftly plugin that allows players to send private messages.
+    <br/>
+  </p>
+</p>
+
+<p align="center">
+  <img src="https://img.shields.io/github/downloads/swiftly-solution/pm/total" alt="Downloads"> 
+  <img src="https://img.shields.io/github/contributors/swiftly-solution/pm?color=dark-green" alt="Contributors">
+  <img src="https://img.shields.io/github/issues/swiftly-solution/pm" alt="Issues">
+  <img src="https://img.shields.io/github/license/swiftly-solution/pm" alt="License">
+</p>
+
+---
+
+### Installation 👀
+
+1. Download the newest [release](https://github.com/swiftly-solution/pm/releases).
+2. Perform a drag-and-drop installation.
+
+### Configuring the plugin 🧐
+
+* After installing the plugin, configure the messages in `addons/swiftly/translations/translation.privatemessages.json`.
+* After installing the plugin, you can change the configuration from `addons/swiftly/configs/plugins`.
+
+### Usage 😀
+
+* Console: ```sw_pm <target> <text>```.
+* Chat: ```!pm <target> <text>```.
+
+### Creating A Pull Request 😃
+
+1. Fork the Project
+2. Create your Feature Branch
+3. Commit your Changes
+4. Push to the Branch
+5. Open a Pull Request
+
+### Have ideas/Found bugs? 💡
+Join [Swiftly Discord Server](https://swiftlycs2.net/discord) and send a message in the topic from `📕╎plugins-sharing` of this plugin!
+
+---
---
+++ plugins/pm/core.lua
@@ -0,0 +1,58 @@
+local cmdWithPrefix = {}
+
+AddEventHandler("OnPluginStart", function(event)
+    local splittedPrefixes = string.split(config:Fetch("core.commandPrefixes"), " ")
+    for i=1,#splittedPrefixes do
+        table.insert(cmdWithPrefix, splittedPrefixes[i].."pm")
+    end
+end)
+
+AddEventHandler("OnClientChat", function(event, playerid, text, teamonly)
+    for i=1,#cmdWithPrefix do
+        if text:find(cmdWithPrefix[i]) then
+            event:SetReturn(false)
+            return EventResult.Handled
+        end
+    end
+    return EventResult.Continue
+end)
+
+commands:Register("pm", function(playerid, args, argsCount, silent, prefix)
+    local sender = GetPlayer(playerid)
+    if not sender then return end
+    local sendername = sender:CBasePlayerController().PlayerName
+
+    if argsCount < 2 then
+        ReplyToCommand(playerid, config:Fetch("pm.prefix"), string.format(FetchTranslation("privatemessages.NotEnoughArgs"), prefix))
+        return
+    end
+
+    local receivers = FindPlayersByTarget(args[1], false)
+    if #receivers == 0 then
+        return ReplyToCommand(playerid, config:Fetch("pm.prefix"), FetchTranslation("privatemessages.PlayerNotFound"))
+    end
+
+    local receiver = receivers[1]
+    if receiver == sender then
+        return ReplyToCommand(playerid, config:Fetch("pm.prefix"), FetchTranslation("privatemessages.PlayerSendYourself"))
+    end
+
+    table.remove(args, 1)
+    local message = table.concat(args, " ")
+
+    ReplyToCommand(receiver:GetSlot(), config:Fetch("pm.prefix"), FetchTranslation("privatemessages.RevicedPM"):gsub("{NAME}", sendername):gsub("{TEXT}", message))
+    ReplyToCommand(playerid, config:Fetch("pm.prefix"), FetchTranslation("privatemessages.SentPM"):gsub("{NAME}", receiver:CBasePlayerController().PlayerName):gsub("{TEXT}", message))
+end)
+
+function GetPluginAuthor()
+    return "Swiftly Solution"
+end
+function GetPluginVersion()
+    return "v1.0.0"
+end
+function GetPluginName()
+    return "Swiftly Private Messages"
+end
+function GetPluginWebsite()
+    return "https://github.com/swiftly-solution/pm"
+end
